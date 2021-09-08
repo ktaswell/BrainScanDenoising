542eacf29f0d9fcbef677080f54e628a3209ed6a
ktaswell <ktaswell@bhavi.us>
2021-09-07 23:36:49 -0700

    Rough scaffold/stub for BrainScanDemo code

---
+++ BrainScanDemo/TestImage.m
@@ -0,0 +1,18 @@
+function [N, D, snrN, snrD] = TestImage(I)
+% TestSlice - Tests a single 2D image for accuracy of denoising via Optimal
+% Hard Thresholding.
+%
+%       Inputs:
+%           I - 2D image matrix of intensity values
+%       
+%       Outputs:
+%           N - Noisey 2D image matrix of I with gaussian noise added
+%           D - Denoised 2D image matrix of N with Optimal Hard
+%           Thresholding
+%           snrN - Signal to Noise Ratio of Noisey 2D Image matrix
+%           snrD - Signal to Noise Ratio of Denoised 2D Image matrix
+
+
+
+
+end 
---
+++ BrainScanDemo/TestVolume.m
@@ -0,0 +1,29 @@
+function [Nvol, Dvol, snrNvec, snrDvec] = TestVolume(Vol)
+% TestVolume - Tests a volume for Optimal Hard Threshold denoising along the 3rd dimension 
+%
+%       Inputs:
+%           Vol - 3D image matrix of intensity values to be tested
+%
+%       Outputs: 
+%           Nvol - 3D image matrix of Vol with noise added at each slice
+%           Dvol - 3D image matrix of Nvol with Optimal Hard Thresholding
+%           applied at each slice
+%           snrNvec - 1D vector of signal to noise ratios of each noisey 2D
+%           image slice in Nvol
+%           snrDvec - 1D vector of signal to noise ratios of each denoised
+%           2D image slice in Dvol
+%
+
+%Initialize data structures with 0s
+n = size(Vol);
+Nvol = zeros(n);
+Dvol = zeros(n);
+
+snrNvec = zeros(n(1), 1);
+snrDvec = zeros(n(1), 1);
+
+for i = 1:n(1)
+    [Nvol(:, :, i), Dvol(:, :, i), snrNvec(i), snrDvec(i)] = TestImage(Vol(:, :, i));
+end
+
+end
